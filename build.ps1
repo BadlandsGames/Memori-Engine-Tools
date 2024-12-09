52f514aa3179a67d1691c8abffcfa5a6c2386bff
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-09 15:04:33 -0800

    made changes

--- build.ps1
+++ build.ps1
@@ -2,39 +2,47 @@ Write-Host "Enter your operating system:"
 Write-Host "win: Windows"
 Write-Host "linux: Linux"
 $choice_os = Read-Host -Prompt "choice"
+$choice_work = $false
 
-# Memori Shader Tool
-cd MemoriShaderTool
-Clear-Host
 if($choice_os -eq "win") {
-    Clear-Host
-    cl /EHsc compiler.cpp
-    ren compiler.exe MemoriShaderTool.exe
-    mkdir MemoriShaderTool
-    move MemoriShaderTool.exe MemoriShaderTool\MemoriShaderTool.exe
-    move ShaderConductor.exe MemoriShaderTool\ShaderConductor.exe
-    del compile.cpp
-    del api.hpp
+    $choice_work = $true
 } elseif($choice_os -eq "linux") {
-    Clear-Host
-    g++ -o MemoriShaderTool.elf compiler.cpp
-    git clone https://github.com/microsoft/ShaderConductor.git
-    cd ShaderConductor
-    mkdir Build
-    cd Build
-    cmake -DCMAKE_BUILD_TYPE=Release ..
-    cmake --build .
-    objcopy --input-format=pei-x86-64 --output-format=elf64-x86-64 ShaderConductor.dll ../../ShaderConductor.elf
-    cd ..
-    cd ..
-    mkdir MemoriShaderTool
-    mv MemoriShaderTool.elf MemoriShaderTool\MemoriShaderTool.elf
-    mv ShaderConductor.elf MemoriShaderTool\ShaderConductor.elf
-    rm -rf ShaderConductor
-    rm compile.cpp
-    rm api.hpp
+    $choice_work = $true
 } else {
+    Clear-Host "Error: Operating system does not exist."
+}
+
+if($choice_work) {
+    # Memori Shader Tool
+    cd MemoriShaderTool
     Clear-Host
-    Write-Host "Error: Operating System doesn't exist."
+    if($choice_os -eq "win") {
+        Clear-Host
+        cl /EHsc compiler.cpp
+        ren compiler.exe MemoriShaderTool.exe
+        mkdir MemoriShaderTool
+        move MemoriShaderTool.exe MemoriShaderTool\MemoriShaderTool.exe
+        move ShaderConductor.exe MemoriShaderTool\ShaderConductor.exe
+        del compile.cpp
+        del api.hpp
+    } else {
+        Clear-Host
+        g++ -o MemoriShaderTool.elf compiler.cpp
+        git clone https://github.com/microsoft/ShaderConductor.git
+        cd ShaderConductor
+        mkdir Build
+        cd Build
+        cmake -DCMAKE_BUILD_TYPE=Release ..
+        cmake --build .
+        objcopy --input-format=pei-x86-64 --output-format=elf64-x86-64 ShaderConductor.dll ../../ShaderConductor.elf
+        cd ..
+        cd ..
+        mkdir MemoriShaderTool
+        mv MemoriShaderTool.elf MemoriShaderTool\MemoriShaderTool.elf
+        mv ShaderConductor.elf MemoriShaderTool\ShaderConductor.elf
+        rm -rf ShaderConductor
+        rm compile.cpp
+        rm api.hpp
+    }
 }
 cd ..
