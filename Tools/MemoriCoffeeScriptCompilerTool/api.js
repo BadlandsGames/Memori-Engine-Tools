7cea8ef0e41ae7f570721e5f9917a06825cdbd07
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-09 17:59:32 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/api.js
+++ Tools/MemoriCoffeeScriptCompilerTool/api.js
@@ -5,4 +5,10 @@ class Debug {
     static Error(textin) {
         console.error(textin);
     }
+    static Performance(textin) {
+        if(textin == "fps" || textin == "FPS") {
+            return 1;
+        }
+        return 0;
+    }
 }
