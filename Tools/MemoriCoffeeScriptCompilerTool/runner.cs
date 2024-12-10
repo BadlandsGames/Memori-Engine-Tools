f7f36739f3e7150f58aaeed06c17cca017a56f28
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-09 17:38:22 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/runner.cs
+++ Tools/MemoriCoffeeScriptCompilerTool/runner.cs
@@ -1,10 +1,11 @@
 using System;
+using System.IO;
 using Jint;
 
 public class RunnerClass
 {
     public void Memori_RunJavaScript(string cmd)
     {
-        var engine = new Engine().Execute(cmd);
+        var engine = new Engine().Execute(File.ReadAllText(cmd));
     }
 }
