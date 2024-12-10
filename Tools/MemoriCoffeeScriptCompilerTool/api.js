3efa4c7c562d19798cc736fa2676743f2f368bfe
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-10 15:57:02 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/api.js
+++ Tools/MemoriCoffeeScriptCompilerTool/api.js
@@ -1,14 +1,10 @@
+"use strict";
+
 class Debug {
     static Log(textin) {
         console.log(textin);
     }
     static Error(textin) {
         console.error(textin);
-    }
-    static Performance(textin) {
-        if(textin == "fps" || textin == "FPS") {
-            return 1;
-        }
-        return 0;
     }
 }
