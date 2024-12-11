d75c44e701222aa89ce2caae56bdc9d429fd67b2
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-10 16:38:01 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/api.hpp
+++ Tools/MemoriCoffeeScriptCompilerTool/api.hpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <windows.h>
+#include <fstream>
 #include <string>
 using namespace std;
 
@@ -35,4 +36,32 @@ void coffee2js(string inputcoffee, string outputjs) {
     newcmd.append(" ");
     newcmd.append(outputjs);
     system(newcmd);
+}
+
+void mergefile(string firstfile, string secondfile, string outputfile) {
+    string full_first = "";
+    string full_second = "";
+    string out_maybe = "";
+    string temp = "";
+    string temp2 = "";
+    ifstream First(firstfile);
+    ifstream Second(secondfile);
+    while(getline(First, temp)) {
+        full_first.append(temp);
+        full_first.append("\n");
+        temp = "";
+    }
+    while(getline(Second, temp2)) {
+        full_second.append(temp2);
+        full_second.append("\n");
+        temp2 = "";
+    }
+    First.close();
+    Second.close();
+    out_maybe.append(full_first);
+    out_maybe.append("\n");
+    out_maybe.append(full_second);
+    ofstream OutFile(outputfile);
+    OutFile << out_maybe;
+    OutFile.close();
 }
