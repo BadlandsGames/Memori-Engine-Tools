a89d859dea9f566347f953c011fc2d1bf8e19928
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-10 17:54:46 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/runner.cs
+++ Tools/MemoriCoffeeScriptCompilerTool/runner.cs
@@ -8,10 +8,8 @@ public class RunnerClass
     {
         var engine = new Engine();
         string text_stuff = File.ReadAllText(cmd);
-        /*
         engine.SetValue("log", new Action<object>(Console.WriteLine));
         text_stuff = text_stuff.Replace("console.log", "log");
-        */
         engine.Execute(text_stuff);
     }
 }
