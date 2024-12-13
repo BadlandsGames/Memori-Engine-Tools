a3ac2a6cd217e490544c0c150da8d5665e6444b2
BadlandsGames <145294897+BadlandsGames@users.noreply.github.com>
2024-12-12 17:02:19 -0800

    made changes

--- Tools/MemoriCoffeeScriptCompilerTool/compile.cpp
+++ Tools/MemoriCoffeeScriptCompilerTool/compile.cpp
@@ -1,9 +1,8 @@
 #include "api.hpp"
 
 int main(int argc, char* argv[]) {
-    js2coffee("api.js", "api_generated.coffee");
-    mergefile("api_generated.coffee", argv[1], "output_generated.coffee");
+    mergefile("api.coffee", argv[1], "output_generated.coffee");
     coffee2js("output_generated.coffee", argv[2]);
-    system("del api_generated.coffee && del output_generated.coffee");
+    system("del output_generated.coffee");
     return 0;
 }
